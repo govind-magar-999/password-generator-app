1530bc7bc1b4d10dbe663a58cb42faadf7fa8aca
Govind Magar <govind.magar21@gmail.com>
2022-12-10 16:25:18 +0530

    Added copy to clipboard functionality

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ let passwordArr = [];
 let regex = (('A'||'B'||'C'||'D'||'E'||'F'||'G'||'H'||'I'||'J'||'K'||'L'||'M'||'N'||'O'||'P'||'Q'||'R'||'S'||'T'||'U'||'V'||'W'||'X'||'Y'||'Z')&&('a'||'b'||'c'||'d'||'e'||'f'||'g'||'h'||'i'||'j'||'k'||'l'||'m'||'n'||'o'||'p'||'q'||'r'||'s'||'t'||'t'||'u'||'v'||'w'||'x'||'y'||'z')&&(1||2||3||4||5||6||7||8||9||0)&&('!'||'@'||'$'||'%'||'^'||'&'||'*'))
 let final_pwd = '';
 clipboardEl.addEventListener('click', () => {
-    
+    resultEl.select();
 })
 
 let checkArr = [];
@@ -66,6 +66,7 @@ function generatePassword(pwdlength, ...elements) {
     }
 }
 
-clipboardEl.addEventListener('click', (e)=>{
-    resultEl.innerText
+clipboardEl.addEventListener('click', (e)=> {
+    navigator.clipboard.writeText(resultEl.innerText);
+    alert("Copied the text: " + resultEl.innerText);
 });
